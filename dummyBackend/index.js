c895b6e5765776ca64f5894bbd7ff9fdc6df6bff
Samin Ahsan <turzo68@gmail.com>
2023-05-28 14:46:38 +0600

    changed vercel config and index.js

--- dummyBackend/index.js
+++ dummyBackend/index.js
@@ -1,31 +1,20 @@
 const jsonServer = require('json-server')
-const clone = require('clone')
 const cors = require('cors')
-const data = require('./db.json')
+const path = require('path')
 
-const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
-
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-    _isFake: isProductionEnv
-})
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
-
-server.use((req, res, next) => {
-    if (req.path !== '/')
-        router.db.setState(clone(data))
-    next()
-})
 
 server.use(cors())
 server.use(jsonServer.bodyParser)
+server.use(middlewares)
 server.use(router)
-server.listen(process.env.PORT || 8000, () => {
-    console.log('JSON Server is running')
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
 })
 
-// Export the Server API
 module.exports = server
